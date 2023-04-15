8e5f9500fdc2403efde13e07c2e713eecf1064ae
Tira <Tirael_god@mail.ru>
2023-04-15 13:15:11 +0300

    Начало проекта

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+// let cursor ={
+//     x:0,
+//     y:0
+// }
+// document.addEventListener("mousemove",(event)=>{
+//     cursor.x = event.offsetX
+//     cursor.y = event.offsetY
+// })
+//
+
+// document.querySelector(".first-img-gif")
+//     .onmouseover = function (){
+//     this.src = '../images/gif/firstProjectGif.gif';
+//     this.width = '100px';
+// }
+// document.querySelector(".first-img-gif")
+//     .onmouseout = function (){
+//     this.src = '../images/firstProject.png';
+//     this.width = '100px';
+// }
+
+
